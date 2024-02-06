3532c9ad23657698afefa7039d7cffe9e257d12a
utkarsh-1602 <usking619@gmail.com>
2024-02-06 17:52:08 +0530

    readme updated

---
+++ README.md
@@ -0,0 +1,2 @@
+# Real-time Polling Application with Chat Feature
+
---
+++ client/src/store/index.js
@@ -0,0 +1,27 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './reducers';
+
+const DEFAULT_STATE = {
+  auth: { isAuthenticated: false },
+  error: { message: null },
+  polls: [],
+  currentPoll: {
+    _id: '5b086e20f7d2381502ce0e46',
+    options: [],
+    question: 'test_poll',
+  },
+};
+
+// Define composeEnhancers for Redux DevTools extension
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Create Redux store with middleware and DevTools extension
+export const store = createStore(
+  rootReducer,
+  DEFAULT_STATE,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+);
