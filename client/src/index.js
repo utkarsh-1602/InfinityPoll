e22813c601abbccec2a08295d211895a01dfe9cf
utkarsh-1602 <usking619@gmail.com>
2024-02-08 17:59:25 +0530

    client build added

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
 
 render(<App />, document.getElementById('root'));
-registerServiceWorker();
